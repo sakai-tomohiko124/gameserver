61a45b571d84f83abc4460d8e45b9862e81c0b1c
sakai-tomohiko124 <your-email@example.com>
2025-10-19 21:19:43 +0900

    A

--- functions/api.py
+++ functions/api.py
@@ -1040,5 +1040,3 @@ def add_csp_headers(response):
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8080, debug=True)
 
-# FlaskアプリケーションをNetlify Functionsとしてエクスポート
-app = WSGIHandler(app)
